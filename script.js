5bba34d393f57540e059700712e91c034024be11
Prabin Magar <psmagar123451@gmail.com>
2021-09-05 16:42:36 +0545

    Source Code

---
+++ script.js
@@ -0,0 +1,9 @@
+// init Isotope
+var $grid = $('#product-list').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
